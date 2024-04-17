3c4cdf5fccc142798f7e3e89556652ae1eadfb3a
alencar-felipe <alencar.felipe.paiva@gmail.com>
2024-04-17 17:18:53 +0200

    mnist_clean generalization

--- sw/mnist_clean/network.c
+++ sw/mnist_clean/network.c
@@ -1,187 +1,136 @@
 #include "network.h"
 
-#define CONV1_ON   ( 16) // Number of Outputs               
-#define CONV1_OY   ( 11) // Output Height
-#define CONV1_OX   ( 11) // Output Width
-#define CONV1_IN   (  1) // Number of Inputs
-#define CONV1_IY   ( 24) // Input Height
-#define CONV1_IX   ( 24) // Input Width
-#define CONV1_WY   (  4) // Weight Height (Kernel Height)
-#define CONV1_WX   (  4) // Weight Width (Kernel Width)
-#define CONV1_SY   (  2) // Input Stride Y
-#define CONV1_SX   (  2) // Input Stride X
-#define CONV1_BIAS (128) // Bias Value
+// CONV1
+#define L0_LON   ( 16) // Number of Outputs
+#define L0_LOY   ( 11) // Output Height
+#define L0_LOX   ( 11) // Output Width
+#define L0_LIN   (  1) // Number of Inputs
+#define L0_LIY   ( 24) // Input Height
+#define L0_LIX   ( 24) // Input Width
+#define L0_LWY   (  4) // Weight Height (Kernel Height)
+#define L0_LWX   (  4) // Weight Width (Kernel Width)
+#define L0_SY    (  2) // Input Stride Y
+#define L0_SX    (  2) // Input Stride X
+#define L0_BIAS  (128) // Bias Value
+#define L0_SHIFT (  8) // Output Left Shift
 
-#define CONV1_O_SIZE (CONV1_ON*CONV1_OY*CONV1_OX)
-#define CONV1_I_SIZE (CONV1_IN*CONV1_IY*CONV1_IX)
-#define CONV1_W_SIZE (CONV1_ON*CONV1_WX*CONV1_WY*CONV1_IN)
+#define L0_O_SIZE (L0_LON*L0_LOY*L0_LOX)
+#define L0_I_SIZE (L0_LIN*L0_LIY*L0_LIX)
+#define L0_W_SIZE (L0_LON*L0_LWX*L0_LWY*L0_LIN)
 
-#define CONV2_ON   ( 24) // Number of Outputs               
-#define CONV2_OY   (  4) // Output Height
-#define CONV2_OX   (  4) // Output Width
-#define CONV2_IN   ( 16) // Number of Inputs
-#define CONV2_IX   ( 11) // Input Height
-#define CONV2_IY   ( 11) // Input Width
-#define CONV2_WY   (  5) // Weight Height (Kernel Height)
-#define CONV2_WX   (  5) // Weight Width (Kernel Width)
-#define CONV2_SY   (  2) // Input Stride Y
-#define CONV2_SX   (  2) // Input Stride X
-#define CONV2_BIAS (128) // Bias Value
+// CONV2
+#define L1_LON   ( 24) // Number of Outputs
+#define L1_LOY   (  4) // Output Height
+#define L1_LOX   (  4) // Output Width
+#define L1_LIN   ( 16) // Number of Inputs
+#define L1_LIX   ( 11) // Input Height
+#define L1_LIY   ( 11) // Input Width
+#define L1_LWY   (  5) // Weight Height (Kernel Height)
+#define L1_LWX   (  5) // Weight Width (Kernel Width)
+#define L1_SY    (  2) // Input Stride Y
+#define L1_SX    (  2) // Input Stride X
+#define L1_BIAS  (128) // Bias Value
+#define L1_SHIFT (  8) // Output Left Shift
 
-#define CONV2_O_SIZE (CONV2_ON*CONV2_OY*CONV2_OX)
-#define CONV2_I_SIZE (CONV2_IN*CONV2_IY*CONV2_IX)
-#define CONV2_W_SIZE (CONV2_ON*CONV2_WX*CONV2_WY*CONV2_IN)
+#define L1_O_SIZE (L1_LON*L1_LOY*L1_LOX)
+#define L1_I_SIZE (L1_LIN*L1_LIY*L1_LIX)
+#define L1_W_SIZE (L1_LON*L1_LWX*L1_LWY*L1_LIN)
 
-#define FC1_O    (150) // Number of Outputs
-#define FC1_I    (384) // Number of Inputs
-#define FC1_BIAS (128) // Bias Value
+// FC1
+#define L2_LON   (150) // Number of Outputs
+#define L2_LOY   (  1) // Output Height
+#define L2_LOX   (  1) // Output Width
+#define L2_LIN   (384) // Number of Inputs
+#define L2_LIX   (  1) // Input Height
+#define L2_LIY   (  1) // Input Width
+#define L2_LWY   (  1) // Weight Height (Kernel Height)
+#define L2_LWX   (  1) // Weight Width (Kernel Width)
+#define L2_SY    (  1) // Input Stride Y
+#define L2_SX    (  1) // Input Stride X
+#define L2_BIAS  (128) // Bias Value
+#define L2_SHIFT (  8) // Output Left Shift
 
-#define FC2_O    (  10) // Number of Outputs
-#define FC2_I    ( 150) // Number of Inputs
-#define FC2_BIAS (1024) // Bias Value
+#define L2_O_SIZE (L2_LON*L2_LOY*L2_LOX)
+#define L2_I_SIZE (L2_LIN*L2_LIY*L2_LIX)
+#define L2_W_SIZE (L2_LON*L2_LWX*L2_LWY*L2_LIN)
 
-static uint8_t conv1_output[CONV1_O_SIZE];
-static uint8_t conv2_output[CONV2_O_SIZE];
-static uint8_t fc1_output[FC1_O];
-static uint8_t fc2_output[FC2_O];
+// FC2
+#define L3_LON   (  10) // Number of Outputs
+#define L3_LOY   (   1) // Output Height
+#define L3_LOX   (   1) // Output Width
+#define L3_LIN   ( 150) // Number of Inputs
+#define L3_LIX   (   1) // Input Height
+#define L3_LIY   (   1) // Input Width
+#define L3_LWY   (   1) // Weight Height (Kernel Height)
+#define L3_LWX   (   1) // Weight Width (Kernel Width)
+#define L3_SY    (   1) // Input Stride Y
+#define L3_SX    (   1) // Input Stride X
+#define L3_BIAS  (1024) // Bias Value
+#define L3_SHIFT (  11) // Output Left Shift
 
-static void conv1(
-    const uint8_t *input,
-          uint8_t *output,
-    const int8_t  *weight
+#define L3_O_SIZE (L3_LON*L3_LOY*L3_LOX)
+#define L3_I_SIZE (L3_LIN*L3_LIY*L3_LIX)
+#define L3_W_SIZE (L3_LON*L3_LWX*L3_LWY*L3_LIN)
+
+static uint8_t l0_out[L0_O_SIZE];
+static uint8_t l1_out[L1_O_SIZE];
+static uint8_t l2_out[L2_O_SIZE];
+static uint8_t l3_out[L3_O_SIZE];
+
+static inline void conv(
+    uint8_t *const output,
+    const uint8_t *const input,
+    const int8_t *const weight,
+    const size_t lon,
+    const size_t loy,
+    const size_t lox,
+    const size_t lin,
+    const size_t liy,
+    const size_t lix,
+    const size_t lwy,
+    const size_t lwx,
+    const size_t sx,
+    const size_t sy,
+    const int32_t bias,
+    const size_t shift
 )
 {
-    for (size_t oy = 0; oy < CONV1_OX; oy++) {
-        for (size_t ox = 0; ox < CONV1_OY; ox++) {
-            for (size_t on = 0; on < CONV1_ON; on++) {
-                
-                size_t o = (oy*CONV1_OX + ox)*CONV1_ON + on;
-                
-                int32_t sum = CONV1_BIAS;
-
-                for (size_t wy = 0; wy < CONV1_WY; wy++) {
-                    for (size_t wx = 0; wx < CONV1_WX; wx++) {
-                        for (size_t in = 0; in < CONV1_IN; in++) {
-                            size_t iy = CONV1_SY*oy + wy;
-                            size_t ix = CONV1_SX*ox + wx;
-                            
-                            size_t i = (iy*CONV1_IX + ix)*CONV1_IN + in;
-                            size_t w = ((on*CONV1_WY + wy)*CONV1_WX + wx)*CONV1_IN + in;
-        
+    for (size_t oy = 0; oy < loy; oy++) {
+        for (size_t ox = 0; ox < lox; ox++) {
+            for (size_t on = 0; on < lon; on++) {
+                size_t o = (oy*lox + ox)*lon + on;
+                int32_t sum = bias;
+                for (size_t wy = 0; wy < lwy; wy++) {
+                    for (size_t wx = 0; wx < lwx; wx++) {
+                        for (size_t in = 0; in < lin; in++) {
+                            size_t iy = sy*oy + wy;
+                            size_t ix = sy*ox + wx;
+                            size_t i = (iy*lix + ix)*lin + in;
+                            size_t w = ((on*lwy + wy)*lwx + wx)*lin + in;
                             sum += input[i] * weight[w];
                         }
                     }
                 }
 
                 // activation
-                if (sum > 0) {
-                    output[o] = (sum >> 8) & 0xFF;
-                } else {
-                    output[o] = 0;
-                }
+                sum = (sum > 0) ? sum : 0;
+                sum >>= shift;
+                output[o] = (uint8_t) sum;
             }
         }
     }
-}
-
-static void conv2(
-    const uint8_t *input,
-          uint8_t *output,
-    const int8_t  *weight
-)
-{
-    for (size_t oy = 0; oy < CONV2_OX; oy++) {
-        for (size_t ox = 0; ox < CONV2_OY; ox++) {
-            for (size_t on = 0; on < CONV2_ON; on++) {
-                
-                size_t o = (oy*CONV2_OX + ox)*CONV2_ON + on;
-                
-                int32_t sum = CONV2_BIAS;
-
-                for (size_t wy = 0; wy < CONV2_WY; wy++) {
-                    for (size_t wx = 0; wx < CONV2_WX; wx++) {
-                        for (size_t in = 0; in < CONV2_IN; in++) {
-                            size_t iy = CONV2_SY*oy + wy;
-                            size_t ix = CONV2_SX*ox + wx;
-                            
-                            size_t i = (iy*CONV2_IX + ix)*CONV2_IN + in;
-                            size_t w = ((on*CONV2_WY + wy)*CONV2_WX + wx)*CONV2_IN + in;
-        
-                            sum += input[i] * weight[w];
-                        }
-                    }
-                }
-
-                // activation
-                if (sum > 0) {
-                    output[o] = (sum >> 8) & 0xFF;
-                } else {
-                    output[o] = 0;
-                }
-            }
-        }
-    }
-}
-
-static void fc1(
-    const uint8_t *input,
-          uint8_t *output,
-    const int8_t  *weight
-)
-{
-    for (size_t o = 0; o < FC1_O; o++) {
-        int32_t sum = FC1_BIAS;
-
-        for (size_t i = 0; i < FC1_I; i++) {
-            size_t w = o*FC1_I + i;
-            sum += input[i] * weight[w];
-        }
-
-        // activation
-        if (sum > 0) {
-            output[o] = (sum >> 8) & 0xFF;
-        } else {
-            output[o] = 0;
-        }
-    }    
-}
-
-static void fc2(
-    const uint8_t *input,
-          uint8_t *output,
-    const int8_t  *weight
-)
-{
-    for (size_t o = 0; o < FC2_O; o++) {
-        int32_t sum = FC2_BIAS;
-
-        for (size_t i = 0; i < FC2_I; i++) {
-            size_t w = o*FC2_I + i;
-            sum += input[i] * weight[w];
-        }
-
-        // activation
-        sum = (sum >> 11);
-        if (sum < 0) {
-            output[o] =  0;
-        } else if (sum > 255) { // 2047
-            output[o] = 255; // 2047
-        } else {
-            output[o] = sum;
-        }
-    }    
 }
 
 static void argmax1(
     const uint8_t *input,
           int32_t *output,
           uint8_t *max
-) 
+)
 {
     *output = 0;
     *max = input[*output];
 
-    for(size_t i = 1; i < FC2_O; i++) {
+    for(size_t i = 1; i < L3_O_SIZE; i++) {
         if(input[i] > *max) {
             *output = i;
             *max = input[i];
@@ -197,43 +146,107 @@ void inference(const uint8_t* input, int32_t* output, uint8_t* credence)
     crc32_table_init();
 
     crc = 0;
-    crc32(&crc, input, 576);
+    crc32(&crc, input, L0_I_SIZE);
     ASSERT(crc == 0x4dfde263);
 #endif
 
-    conv1(input, conv1_output, conv1_weight);
+    conv(
+        l0_out,
+        input,
+        conv1_weight,
+        L0_LON,
+        L0_LOY,
+        L0_LOX,
+        L0_LIN,
+        L0_LIY,
+        L0_LIX,
+        L0_LWY,
+        L0_LWX,
+        L0_SX,
+        L0_SY,
+        L0_BIAS,
+        L0_SHIFT
+    );
 
 #ifdef VALIDATION_RUN
     crc = 0;
-    crc32(&crc, conv1_output, CONV1_O_SIZE);
+    crc32(&crc, l0_out, L0_O_SIZE);
     ASSERT(crc == 0xa6062dba);
 #endif
 
-    conv2(conv1_output, conv2_output, conv2_weight);
-
-#ifdef VALIDATION_RUN    
-    crc = 0;
-    crc32(&crc, conv2_output, CONV2_O_SIZE);
-    ASSERT(crc == 0x0aa1524f);
-#endif
-
-    fc1(conv2_output, fc1_output, fc1_weight);
-
-#ifdef VALIDATION_RUN 
-    crc = 0;
-    crc32(&crc, fc1_output, FC1_O);
-    ASSERT(crc == 0x7e1d772e);
-#endif
-
-    fc2(fc1_output, fc2_output, fc2_weight);
+    conv(
+        l1_out,
+        l0_out,
+        conv2_weight,
+        L1_LON,
+        L1_LOY,
+        L1_LOX,
+        L1_LIN,
+        L1_LIY,
+        L1_LIX,
+        L1_LWY,
+        L1_LWX,
+        L1_SX,
+        L1_SY,
+        L1_BIAS,
+        L1_SHIFT
+    );
 
 #ifdef VALIDATION_RUN
     crc = 0;
-    crc32(&crc, fc2_output, FC2_O);
+    crc32(&crc, l1_out, L1_O_SIZE);
+    ASSERT(crc == 0x0aa1524f);
+#endif
+
+    conv(
+        l2_out,
+        l1_out,
+        fc1_weight,
+        L2_LON,
+        L2_LOY,
+        L2_LOX,
+        L2_LIN,
+        L2_LIY,
+        L2_LIX,
+        L2_LWY,
+        L2_LWX,
+        L2_SX,
+        L2_SY,
+        L2_BIAS,
+        L2_SHIFT
+    );
+
+#ifdef VALIDATION_RUN
+    crc = 0;
+    crc32(&crc, l2_out, L2_O_SIZE);
+    ASSERT(crc == 0x7e1d772e);
+#endif
+
+    conv(
+        l3_out,
+        l2_out,
+        fc2_weight,
+        L3_LON,
+        L3_LOY,
+        L3_LOX,
+        L3_LIN,
+        L3_LIY,
+        L3_LIX,
+        L3_LWY,
+        L3_LWX,
+        L3_SX,
+        L3_SY,
+        L3_BIAS,
+        L3_SHIFT
+    );
+
+#ifdef VALIDATION_RUN
+    crc = 0;
+    crc32(&crc, l3_out, L3_O_SIZE);
     ASSERT(crc == 0x6d779679);
 #endif
 
-    argmax1(fc2_output, output, credence);
+    argmax1(l3_out, output, credence);
 
 #ifdef VALIDATION_RUN
     crc = 0;
@@ -242,8 +255,3 @@ void inference(const uint8_t* input, int32_t* output, uint8_t* credence)
 #endif
 
 }
-
-
-
-
-
